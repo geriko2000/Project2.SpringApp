8e81f79a93dd016f337ca56dd81cdf052fde0e72
geriko2000 <geriko2000@yanex.ru>
2020-11-27 18:49:51 +0700

    My test project with spring

---
+++ src/main/java/com/example/Project2/SpringApp/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.Project2.SpringApp.repos;
+
+import com.example.Project2.SpringApp.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String Username);
+}
