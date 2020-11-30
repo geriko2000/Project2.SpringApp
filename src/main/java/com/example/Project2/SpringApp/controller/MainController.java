b79d67a1fdb2dd0ed2307519eb7d149dae10c3f7
geriko2000 <geriko2000@yanex.ru>
2020-11-30 17:37:18 +0700

    My test project with spring

--- src/main/java/com/example/Project2/SpringApp/controller/MainController.java
+++ src/main/java/com/example/Project2/SpringApp/controller/MainController.java
@@ -26,9 +26,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Map<String, Object> model){
         Iterable<Message> messages = messageRepo.findAll();
+
+        if(filter != null && !filter.isEmpty()){
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
         model.put("messages", messages);
+        model.put("filter", filter);
         return "main";
     }
 
@@ -45,21 +53,7 @@ public class MainController {
 
         model.put("messages", messages);
 
-        return "main";
-
+        return "redirect:/main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-        Iterable<Message> messages;
-        if(filter != null && !filter.isEmpty()){
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
 }
